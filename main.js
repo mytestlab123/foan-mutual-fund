1eb68740389e35e5796648e3ddef07699109e01d
Amit Karpe <amitkarpe@gmail.com>
2022-06-03 16:57:48 +0800

    Added support for MATIC and other stable coins like USDC and USDT

--- main.js
+++ main.js
@@ -4,12 +4,22 @@ Moralis.serverURL = "https://prylzpawi05g.usemoralis.com:2053/server"
 let currentTrade = {};
 let currentSelectSide;
 let tokens;
+let myObj;
+let MATIC = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
+let USDC  = '0x2791bca1f2de4661ed88a30c99a7a9449aa84174'
+let USDT  = '0xc2132d05d31c914a87c6611c10748aeb04b58e8f'
+let DAI   = '0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3'
 
+const fund = new Map([
+    ['FTM','0xb85517b87bf64942adf3a0b9e4c71e4bc5caa4e5'],
+    ['ONE','0x80c0cbdb8d0b190238795d376f0bd57fd40525f2']
+  ]);
 async function init(){
     await Moralis.initPlugins();
     // await Moralis.enable();
     await Moralis.enableWeb3();
     await listAvailableTokens();
+    // await setMatic();
     currentUser = Moralis.User.current();
     if(currentUser){
         document.getElementById("swap_button").disabled = false;
@@ -23,25 +33,57 @@ async function listAvailableTokens(){
     tokens = result.tokens;
     let parent = document.getElementById("token_list");
     for( const address in tokens){
-        let token = tokens[address];
+        if (address == MATIC || address == USDC ) {
+            let token = tokens[address];
+            console.log ("Successful ✅");
+            // console.log (address);
+            document.getElementById("test").innerHTML = address;
+            let div = document.createElement("div");
+            div.setAttribute("data-address", address)
+            div.className = "token_row";
+            let html = `
+            <img class="token_list_img" src="${token.logoURI}">
+            <span class="token_list_text">${token.symbol}</span>
+            `
+            div.innerHTML = html;
+            div.onclick = (() => {selectToken(address)});
+            parent.appendChild(div);
+        }
+    }
+}
+
+
+async function setMatic(){
+    // {symbol: 'MATIC', name: 'MATIC', decimals: 18, address: '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee', logoURI: 'https://tokens.1inch.io/0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0.png'}
+    // const result = await Moralis.Plugins.oneInch.getSupportedTokens({
+    //     chain: 'polygon', // The blockchain you want to use (eth/bsc/polygon)
+    //   });
+    // tokens = result.tokens;
+    let parent = document.getElementById("token_list");
+    // for( const address in tokens){
+        let token = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
         let div = document.createElement("div");
         div.setAttribute("data-address", address)
         div.className = "token_row";
         let html = `
-        <img class="token_list_img" src="${token.logoURI}">
-        <span class="token_list_text">${token.symbol}</span>
+        <img class="token_list_img" src="https://tokens.1inch.io/0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0.png">
+        <span class="token_list_text">MATIC</span>
         `
         div.innerHTML = html;
         div.onclick = (() => {selectToken(address)});
         parent.appendChild(div);
-    }
+    // }
+
 }
 
 function selectToken(address){
     closeModal();
-    console.log(tokens);
+    // console.log(tokens);
     currentTrade[currentSelectSide] = tokens[address];
-    console.log(currentTrade);
+    myObj = {"currentTrade": currentTrade};
+    console.log(myObj);
+    // console.log(currentTrade[currentSelectSide]);
+    // console.log("currentTrade:" currentTrade);
     renderInterface();
     getQuote();
 }
@@ -119,28 +161,19 @@ async function trySwap(){
         }
     }
     try {
-        // let receipt = await doSwap(address, amount);
-        // ONE,0x80c0cbdb8d0b190238795d376f0bd57fd40525f2
-        let receipt1 = await doSwap1(address, amount, "0x80c0cbdb8d0b190238795d376f0bd57fd40525f2");
         // alert(JSON.stringify(receipt1));
-        alert('ONE');
-        console.log(JSON.stringify(receipt1));
-        // FTM,0xb85517b87bf64942adf3a0b9e4c71e4bc5caa4e5
-        let receipt2 = await doSwap1(address, amount, "0xb85517b87bf64942adf3a0b9e4c71e4bc5caa4e5");
-        alert('FTM');
-        console.log(JSON.stringify(receipt2));
-        // ATOM,0xac51C4c48Dc3116487eD4BC16542e27B5694Da1b
+        let receipt1 = await doSwap1(address, amount, "0xb85517b87bf64942adf3a0b9e4c71e4bc5caa4e5");
+        alert('FTM,0xb85517b87bf64942adf3a0b9e4c71e4bc5caa4e5');
+
+        let receipt2 = await doSwap1(address, amount, "0x80c0cbdb8d0b190238795d376f0bd57fd40525f2");
+        alert('ONE,0x80c0cbdb8d0b190238795d376f0bd57fd40525f2');
+
         let receipt3 = await doSwap1(address, amount, "0xac51C4c48Dc3116487eD4BC16542e27B5694Da1b");
-        alert('ATOM');
-        console.log(JSON.stringify(receipt3));
-        // METIS,0xA863246658DEA34111C3C1DceDb2cfd5d6067334
-        let receipt4 = await doSwap1(address, amount, "0xA863246658DEA34111C3C1DceDb2cfd5d6067334");
-        console.log(JSON.stringify(receipt4));
-        alert("METIS");
-        // SAND,0xBbba073C31bF03b8ACf7c28EF0738DeCF3695683
-        let receipt5 = await doSwap1(address, amount, "0xBbba073C31bF03b8ACf7c28EF0738DeCF3695683");
-        alert("SAND");
-        console.log(JSON.stringify(receipt5));
+        alert('ATOM,0xac51C4c48Dc3116487eD4BC16542e27B5694Da1b');
+
+        let receipt4 = await doSwap1(address, amount, "0x72bd80445b0db58ebe3e8db056529d4c5faf6f2f");
+        alert("NEAR,0x72bd80445b0db58ebe3e8db056529d4c5faf6f2f");
+
         alert("Investment is completed.");
     
     } catch (error) {
@@ -160,16 +193,6 @@ async function doSwap1(userAddress, amount, toaddress){
       });
 }
 
-// function doSwap(userAddress, amount){
-//     return Moralis.Plugins.oneInch.swap({
-//         chain: 'polygon', // The blockchain you want to use (eth/bsc/polygon)
-//         fromTokenAddress: currentTrade.from.address, // The token you want to swap
-//         toTokenAddress: currentTrade.to.address, // The token you want to receive
-//         amount: amount,
-//         fromAddress: userAddress, // Your wallet address
-//         slippage: 1,
-//       });
-// }
 async function logOut() {
     await Moralis.User.logOut();
     console.log("logged out");
@@ -177,8 +200,8 @@ async function logOut() {
 
 init();
 
-// document.getElementById("modal_close").onclick = closeModal;
-// document.getElementById("from_token_select").onclick = (() => {openModal("from")});
+document.getElementById("modal_close").onclick = closeModal;
+document.getElementById("from_token_select").onclick = (() => {openModal("from")});
 // document.getElementById("to_token_select").onclick = (() => {openModal("to")});
 document.getElementById("login_button").onclick = login;
 // document.getElementById("from_amount").onblur = getQuote;
